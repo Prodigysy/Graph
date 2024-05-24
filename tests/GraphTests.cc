08dbc1eed4aa4f81be6a32187a44ced77332035e
Prodigysy <miracle.owsyannikov@yandex.ru>
2024-05-24 22:02:08 +0400

    add one more test

--- tests/GraphTests.cc
+++ tests/GraphTests.cc
@@ -25,3 +25,31 @@ TEST_F(GraphTest, AddVertex) {
     ASSERT_TRUE(graph.has_vertex("D"));
 }
 
+TEST_F(GraphTest, AddRemoveEdge) {
+    ASSERT_TRUE(graph.has_edge("A", "B"));
+    ASSERT_TRUE(graph.has_edge("A", "C"));
+    ASSERT_TRUE(graph.has_edge("B", "D"));
+    ASSERT_TRUE(graph.has_edge("C", "D"));
+
+    ASSERT_TRUE(graph.remove_edge("A", "B"));
+    ASSERT_FALSE(graph.has_edge("A", "B"));
+}
+
+TEST_F(GraphTest, BreadthFirstSearch_PrintResult) {
+    std::vector<std::string> visited_vertices;
+    auto action = [&visited_vertices](const std::string& vertex) {
+        visited_vertices.push_back(vertex);
+        std::cout << vertex << " -> ";
+        };
+
+    std::cout << "BFS traversal result: ";
+    graph.walk("A", action);
+    std::cout << std::endl;
+
+    ASSERT_EQ(visited_vertices.size(), 4);
+    ASSERT_EQ(visited_vertices[0], "A");
+    ASSERT_EQ(visited_vertices[1], "B");
+    ASSERT_EQ(visited_vertices[2], "C");
+    ASSERT_EQ(visited_vertices[3], "D");
+}
+
